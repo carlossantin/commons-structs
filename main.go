cf9aa64d521298651c2fb0d5fe1a7c554d8634c7
Carlos Eduardo Santin <cesantin@gmail.com>
2025-05-20 10:37:35 -0300

    Add echo

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import authz "github.com/carlossantin/resource-policy-framework/auth"
+import (
+	authz "github.com/carlossantin/resource-policy-framework/auth"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+	e := echo.New()
+
 	r := authz.Role{}
 	println(r.ID)
+
+	e.Start(":8080")
+	println("Server started on port 8080")
 }
